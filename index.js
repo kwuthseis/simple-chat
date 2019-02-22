1816925bb6b637256f7353cd1a3076b4c9355035
Klauss F. Wuth Aedo <kwuthseis@gmail.com>
2019-02-22 12:21:38 -0300

    Fin del tutorial socket IO

--- index.js
+++ index.js
@@ -7,8 +7,12 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
     socket.on('chat message',function(msg) {
-        console.log('Message: ' + msg);
+        io.emit('chat message', msg);
     });
 });
 
